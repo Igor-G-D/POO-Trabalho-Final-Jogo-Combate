0d3d1279f1bf129cb510fc65e8165a17ee6e9e51
Igor-G-D <igorgomesdutra@gmail.com>
2023-05-05 02:20:14 -0300

    FInished the moveOrAttack method on Board Class

--- JogoCombate/src/game/Board.java
+++ JogoCombate/src/game/Board.java
@@ -2,6 +2,8 @@ package game;
 
 import java.util.Random;
 
+import java.lang.Math;
+
 import javax.swing.CellEditor;
 
 public class Board {
@@ -66,49 +68,90 @@ public class Board {
             return false;
         }
 
-        if(playerAction && start.getPiece().getPlayerOwned()) { // player acting upon a player piece
-            if(destination.getPiece() == null) { // attempting to move
-                boolean canMove = start.getPiece().canMoveTo(start.getPosx(), start.getPosy(), destination.getPosx(), destination.getPosy()); // returns if the peice can theoretically move to that space
-                
-                if(!canMove) {
-                    return false; // invalid move
-                }
-                
-                if(start.getPiece().getPieceValue() == 2) { // means it is a soldier, need to check if there are pieces or an obstacle in between
-                    int checkFrom;
-                    int checkTo;
-                    if(start.getPosx() == destination.getPosx()) { // means its moving in the y axis
-                        if(start.getPosy() > destination.getPosy()) {
-                            checkFrom = destination.getPosy();
-                            checkTo = destination.getPosy();
-                        }
-                        for(int i = checkFrom+1 ; i < checkTo; i++) { // checks between two positions, if the start is 2 and the destination is 5, checks positions 3 and 4
-                            if(this.cells[start.getPosx()][i].getIsObstacle() || this.cells[start.getPosx()][i].getPiece() != null) {
-                                return false; // if there is a place in between the start and the destination that is an obstacle or a piece, means that the move is invalid
-                            }
-                        }
-                    } else {// means its moving in the x axis
-                        if(start.getPosx() > destination.getPosx()) {
-                            checkFrom = destination.getPosx();
-                            checkTo = destination.getPosx();
-                        }
-                        for(int i = checkFrom+1 ; i < checkTo; i++) { // checks between two positions, if the start is 2 and the destination is 5, checks positions 3 and 4
-                            if(this.cells[i][start.getPosy()].getIsObstacle() || this.cells[i][start.getPosy()].getPiece() != null) {
-                                return false; // if there is a place in between the start and the destination that is an obstacle or a piece, means that the move is invalid
-                            }
+        if (playerAction && !start.getPiece().getPlayerOwned()) {
+            return false; // TODO: turn this into an exception (player can't act on a non player piece)
+        }
+
+        if(destination.getPiece() == null) { // attempting to move
+            boolean canMove = start.getPiece().canMoveTo(start.getPosx(), start.getPosy(), destination.getPosx(), destination.getPosy()); // returns if the peice can theoretically move to that space
+            
+            if(!canMove) {
+                return false; // invalid move
+            }
+            
+            if(start.getPiece().getPieceValue() == 2) { // means it is a soldier, need to check if there are pieces or an obstacle in between
+                int checkFrom, checkTo;
+                if(start.getPosx() == destination.getPosx()) { // means its moving in the y axis
+                    if(start.getPosy() > destination.getPosy()) { // always check from the lower value to the higher
+                        checkFrom = destination.getPosy();
+                        checkTo = start.getPosy();
+                    } else {
+                        checkFrom = start.getPosy();
+                        checkTo = destination.getPosy();
+                    }
+                    for(int i = checkFrom+1 ; i < checkTo; i++) { // checks between two positions, if the start is 2 and the destination is 5, checks positions 3 and 4
+                        if(this.cells[start.getPosx()][i].getIsObstacle() || this.cells[start.getPosx()][i].getPiece() != null) {
+                            return false; // if there is a place in between the start and the destination that is an obstacle or a piece, means that the move is invalid
                         }
                     }
-                } //  end testing if its a soldier
+                } else {// means its moving in the x axis
+                    if(start.getPosx() > destination.getPosx()) {// always check from the lower value to the higher
+                        checkFrom = destination.getPosx();
+                        checkTo = destination.getPosx();
+                    } else {
+                        checkFrom = start.getPosx();
+                        checkTo = destination.getPosx();
+                    }
+                    for(int i = checkFrom+1 ; i < checkTo; i++) { // checks between two positions, if the start is 2 and the destination is 5, checks positions 3 and 4
+                        if(this.cells[i][start.getPosy()].getIsObstacle() || this.cells[i][start.getPosy()].getPiece() != null) {
+                            return false; // if there is a place in between the start and the destination that is an obstacle or a piece, means that the move is invalid
+                        }
+                    }
+                }
+            } //  end testing if its a soldier
+
+            movePiece(start, destination); //if the piece can move, and there are no objects in between, the piece is removed from the starting position and placed on the destination
+            return true;
+        } else if (isAdjacent(start, destination)) { // if there is a piece in the destination, means it is attacking
+            if (destination.getPiece().getPlayerOwned() == start.getPiece().getPlayerOwned()) {
+                return false; //TODO: make this into an exception (attacking piece of the same team)
             }
-        } else if ( !playerAction && !start.getPiece().getPlayerOwned() ) {
-            
+
+            int result = start.getPiece().attack(destination.getPiece());
+
+            if (result == -1) { // attacking piece lost
+                start.getPiece().setVisibility(true);
+                destination.getPiece().setVisibility(true);
+                
+                start.removePiece();
+            } else if (result == 0) { // tie. both get removed
+                start.getPiece().setVisibility(true);
+                destination.getPiece().setVisibility(true);
+                
+                start.removePiece();
+                destination.removePiece();
+            } else { // only other option is result == 1, means that the attacking piece won
+                start.getPiece().setVisibility(true);
+                destination.getPiece().setVisibility(true);
+
+                destination.removePiece();
+                movePiece(start, destination);// piece that attacks occupies the space of the piece it won against
+            }
         }
 
-        
+        return false; //if it reaches this condition, means that it is trying to attack a piece that is non adjacent, which is an invalid move
+    }
 
+    private void movePiece(Cell start, Cell destination) {
+        destination.placePiece(start.getPiece());
+        start.removePiece();
+    } 
+
+    private boolean isAdjacent(Cell start, Cell destination) {
+        if(Math.abs(start.getPosx() - destination.getPosx()) + Math.abs(start.getPosy() - destination.getPosy()) == 1) { // if adjacent, |x(start) - x(dest)| + |y(start) - y(dest)| since one must be 1, and the other 0
+            return true;
+        } else {
+            return false;
         }
     }
-    
-
-
 }
