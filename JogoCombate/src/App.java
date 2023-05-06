c06d66404220661de3f1f227d2737895f4f0d668
Otávio Rocha <otaviosrocha30@gmail.com>
2023-05-06 17:44:14 -0300

    changed app

--- JogoCombate/src/App.java
+++ JogoCombate/src/App.java
@@ -3,12 +3,9 @@ import graphics.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Board test = new Board();
+        GraphicBoard gb = new GraphicBoard();
 
-        Cell testCell = test.getCell(0,0);
-
-        int test1 = testCell.getPosx();
-        int test2 = testCell.getPosy();
+        gb.showWindow();
 
     }
 }
--- JogoCombate/src/graphics/GraphicBoard.java
+++ JogoCombate/src/graphics/GraphicBoard.java
@@ -19,6 +19,8 @@ public class GraphicBoard extends JFrame {
     
     public void showWindow() {
 
+        //bd.setStartPlayerChoice();
+
         setTitle("Teste");
         setSize(400, 600);
         setLocationRelativeTo(null);
