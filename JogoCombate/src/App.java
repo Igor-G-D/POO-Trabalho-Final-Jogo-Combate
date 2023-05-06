675a5eed82c46edafff0a96102db95e26651b028
Igor-G-D <igorgomesdutra@gmail.com>
2023-05-06 15:16:34 -0300

    fixed bugs related to not initializing matrixes and arrays

--- JogoCombate/src/App.java
+++ JogoCombate/src/App.java
@@ -1,5 +1,13 @@
+import game.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Board test = new Board();
+
+        Cell testCell = test.getCell(0,0);
+
+        int test1 = testCell.getPosx();
+        int test2 = testCell.getPosy();
+
     }
 }
